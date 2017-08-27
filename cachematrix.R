b13d4ea5a87cec532bac428098cd80c986bf980f
shalu143 <31365841+shalu143@users.noreply.github.com>
2017-08-27 16:42:39 +0530

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -43,5 +43,12 @@ cacheSolve <- function(x, ...) {
          if(!is.null(invMatrix)) {                       #if inverse matrix is not NULL
           message("Getting Cached Invertible Matrix")   #Type message: Getting Cached Invertible Matrix 
           return(invMatrix)                             #return the invertible matrix
-         }
+         
+}
+#if value of the invertible matrix is NULL then  
+MatrixData <- x$getMatrix()                     #get the original Matrix Data 
+invMatrix <- solve(MatrixData, ...)             #use solve function to inverse the matrix
+x$setInverse(invMatrix)                         #set the invertible matrix 
+return(invMatrix)                               #return the invertible matrix
+
 }
